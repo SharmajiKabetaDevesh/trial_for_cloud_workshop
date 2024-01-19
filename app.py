4004e62461e4bd0b19e6b6d3a39d9a7b7d7a4b2d
SharmajiKabetaDevesh <124495349+SharmajiKabetaDevesh@users.noreply.github.com>
2024-01-19 16:42:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi,Devesh,Devil & Shubhankar this side doing shit here'
+    return 'Hi,DevRaj here fuck the world ??'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port = 5000)
